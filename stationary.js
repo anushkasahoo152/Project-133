bc29ae5980beb80172defa965d41f36c33f7d3ba
anushkasahoo152 <93137129+anushkasahoo152@users.noreply.github.com>
2021-12-17 16:46:27 +0530

    Add files via upload

---
+++ stationary.js
@@ -0,0 +1,42 @@
+Status = "";
+stationary_img = "";
+objects = [];
+function preload() {
+    stationary_img = loadImage('stationary.jpg');
+}
+function setup() {
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = 'Status: Detecting Objects';
+}
+function modelLoaded() {
+    console.log('Model Loaded!')
+    Status = true;
+    objectDetector.detect(stationary_img , gotResult);
+    }
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}    
+function draw() {
+    if(Status != "") {
+        objectDetector.detect(stationary_img, gotResult);
+        for(i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = 'Status: Object Detected';
+            document.getElementById("object_detected").innerHTML = 'There are 3 objects in the image from which cocossd model has detected '+objects.length+' objects';
+            fill("#FF0000");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label+''+percent+'%', objects[i].x ,objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height); 
+        }
+    }
+}
+function back() {
+    window.location = "index.html"; 
+}
